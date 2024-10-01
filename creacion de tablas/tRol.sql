b59bfb012656601c80249b36bbf55a7d21dcccd3
emimedina32 <emilianomedina357@gmail.com>
2024-10-01 16:56:25 -0300

    .

--- creacion de tablas/tRol.sql
+++ creacion de tablas/tRol.sql
@@ -7,3 +7,11 @@ CREATE TABLE Rol (
     Empleado VARCHAR(50), -- Especificar longitud para VARCHAR
     FOREIGN KEY (UsuarioID) REFERENCES Usuarios(ID)
 );
+
+
+INSERT INTO Rol (UsuarioID, Cliente,
+Empleado)
+VALUES ('1', 'SI','NO' ),
+       ('2', 'NO','SI' ),
+       ('3', 'SI','NO' ),
+       ('4','SI','SI' );
